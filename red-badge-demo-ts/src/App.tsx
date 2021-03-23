fcf82d6315b5e1f444220fc075212fcd6b3a5c25
sarah-reynolds12 <73564744+sarah-reynolds12@users.noreply.github.com>
2021-03-23 19:46:18 -0400

    propsexamples

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import PropsExample from "./components/PropsExample"
+import PropsMapping from "./components/PropsMapping"
 
 function App() {
-  return <div></div>;
+  const visitedPlaces = ["Hawaii", "Ireland", "Mexico", "Bahamas", "London", "Dominican Republic"]
+  return <div>
+    <PropsExample name = "Tom" business ="MySpace" />
+    <PropsMapping visited= {visitedPlaces} />
+  </div>;
 }
 
 export default App;
